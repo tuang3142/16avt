deafcccff7e04c4e9f0c2e7f8b9253789d724452
Nguyen Viet Tuan <tuan.nguyenviet271@gmail.com>
2020-06-28 13:07:16 +0700

    Init project
    Create script to extract important data.

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'https://www.16personalities.com/profiles/5749bc31c9e3d'
+page = requests.get(URL)
+doc = BeautifulSoup(page.content, 'html.parser')
+
+def get_traits(doc):
+    traits = []
+    cards = doc.find(class_='cards').find_all("app-pct-card")
+    for card in cards:
+        traits.append({
+            'title': card.attrs['title'],
+            'label': card.attrs['label'],
+            'score': card.attrs[':score']
+        })
+    return traits
+
+def get_hero_info(doc):
+    def no_question_mark_td(tag):
+        return tag.name == 'td' and not tag.text == '?'
+
+    def trim(text):
+        t = text.strip()
+        if t[-1] == ':':
+            return t[:len(t)-1]
+        return t
+
+    info, pair = [], []
+    table_datas = doc.find(class_='info-table').find_all(no_question_mark_td)
+    for td in table_datas:
+        text = trim(td.text)
+        pair.append(text)
+        if len(pair) == 2:
+            info.append({ pair[0]: pair[1] })
+            pair = []
+    return info
+
+print(get_hero_info(doc))
